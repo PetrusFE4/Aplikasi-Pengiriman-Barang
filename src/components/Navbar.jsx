fc62958a9d2cdd1c66c889bfe01637aa1cc97edb
ajiekusumadhany <ajieikruskusumadhany@gmail.com>
2024-06-05 10:22:06 +0700

    menambah dropdown menu di navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,17 +7,17 @@ import { AiOutlineMenuUnfold } from "react-icons/ai";
 function Header() {
   return (
     <header className="header">
-      <nav class="navbar navbar-expand-lg pt-0 pb-0" id="nav-dekstop">
-        <div class="container-fluid p-xl-2 ps-xl-5 pe-xl-5 fw-semibold">
-          <div class="judul d-flex align-items-center">
-            <a class="navbar-brand" href="#home">
+      <nav className="navbar navbar-expand-lg pt-0 pb-0" id="nav-dekstop">
+        <div className="container-fluid p-xl-2 ps-xl-5 pe-xl-5 fw-semibold">
+          <div className="judul d-flex align-items-center">
+            <a className="navbar-brand" href="#home">
               <h1 className="fw-bold">
                 Febe<span>Express</span>
               </h1>
             </a>
           </div>
           <button
-            class="navbar-toggler custom-toggler"
+            className="navbar-toggler custom-toggler"
             type="button"
             aria-expanded="false"
           >
@@ -25,33 +25,82 @@ function Header() {
               <AiOutlineMenuUnfold size={35} color="#000" />
             </span>
           </button>
-          <div class="collapse navbar-collapse">
-            <ul class="navbar-nav mx-lg-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link " aria-current="page" href="#home">
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mx-lg-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link " aria-current="page" href="#home">
                   Home
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#order">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#order"
+                  id="navbarDropdownHome"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   Order
                 </a>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownHome"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#cek-ongkir">
+                      Cek Ongkir
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#order">
+                      Order
+                    </a>
+                  </li>
+                </ul>
               </li>
 
-              <li class="nav-item">
-                <a class="nav-link" href="#tracking">
+              <li className="nav-item">
+                <a className="nav-link" href="#tracking">
                   Tracking
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#service">
+              <li className="nav-item">
+                <a className="nav-link" href="#service">
                   Service
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#information">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#information"
+                  id="navbarDropdownHome"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   Information
                 </a>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownHome"
+                >
+                  <li>
+                    <a className="dropdown-item" href="support-center">
+                      Support Center
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="call-center">
+                      Call Center
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="faq">
+                      FAQ
+                    </a>
+                  </li>
+                </ul>
               </li>
             </ul>
             <button className="login">Login</button>
@@ -65,21 +114,72 @@ function Header() {
             <IoIosCloseCircle size={35} style={{ color: "#01aa5a" }} />
           </div>
         </div>
-        <ul>
-          <li>
-            <a href="#home">Home</a>
+        <ul className="navbar-nav ps-4 fw-semibold">
+          <li className="nav-item">
+            <a className="nav-link" href="#home">
+              Home
+            </a>
           </li>
-          <li>
-            <a href="#order">Order</a>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#ordermenu"
+              data-bs-toggle="collapse"
+            >
+              Order
+            </a>
+            <ul className="nav collapse" id="ordermenu" data-bs-parent="menu">
+              <li className="nav-item">
+                <a className="nav-link" href="cek-ongkir" aria-current="page">
+                  Cek Ongkir
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="order">
+                  Order
+                </a>
+              </li>
+            </ul>
           </li>
-          <li>
-            <a href="#tracking">Tracking</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#tracking">
+              Tracking
+            </a>
           </li>
-          <li>
-            <a href="#service">Service</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#service">
+              Service
+            </a>
           </li>
-          <li>
-            <a href="#informasi">Informasi</a>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#informationmenu"
+              data-bs-toggle="collapse"
+            >
+              Information
+            </a>
+            <ul
+              className="nav collapse"
+              id="informationmenu"
+              data-bs-parent="menu"
+            >
+              <li className="nav-item">
+                <a className="nav-link" href="support-center">
+                  Support Center
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="call-center">
+                  Call Center
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="faq">
+                  FAQ
+                </a>
+              </li>
+            </ul>
           </li>
         </ul>
         <div className="button-login">
