12ba45451e144a58a6a45af173aa703b2e3c0f48
ajiekusumadhany <ajieikruskusumadhany@gmail.com>
2024-06-08 17:12:08 +0700

    membuat halaman notfound

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Login from "./Auth/Login";
 import Register from "./Auth/Register";
 import CheckShippingCostsPage from "./pages/CheckShippingCostsPage";
 import CallCenter from "./pages/CallCenter";
+import NotFoundPage from "./pages/NotFoundPage";
+
 function App() {
   return (
     <div className="App">
@@ -14,6 +16,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/cek-ongkir" element={<CheckShippingCostsPage />} />
           <Route path="/call-center" element={<CallCenter />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Router>
     </div>
