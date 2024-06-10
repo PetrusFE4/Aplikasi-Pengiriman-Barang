b3e6733e077a94d814136331739016a31ca5841a
ajiekusumadhany <ajieikruskusumadhany@gmail.com>
2024-06-10 15:34:13 +0700

    add icon menu

--- client/src/componentDashboard/NavbarDashboard.jsx
+++ client/src/componentDashboard/NavbarDashboard.jsx
@@ -1,25 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "../assets/css/NavbarDashboard.css";
-import { AiOutlineMenuUnfold } from "react-icons/ai";
-import { Link, useNavigate, useLocation } from "react-router-dom";
-import React, { useState } from 'react';
+import { AiOutlineMenuUnfold, AiOutlineTruck } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { FaHome, FaUsers } from "react-icons/fa";
+import {
+  IoPersonCircleOutline,
+  IoLogOutOutline,
+} from "react-icons/io5";
+import { CiBoxes } from "react-icons/ci";
+import { MdOutlineSettings } from "react-icons/md";
+import { LuUserPlus } from "react-icons/lu";
 
 function NavbarDashboard() {
-  const [userType, setUserType] = useState('customer'); // Ganti dengan logika autentikasi yang sesuai
-
+  const [userType, setUserType] = useState("customer");
   return (
     <header className="header">
-      <nav
-        className="navbar  pt-1 pb-1 pt-sm-0 pb-sm-0"
-        id="nav-dekstop"
-      >
+      <nav className="navbar  pt-1 pb-1 pt-sm-0 pb-sm-0" id="nav-dekstop">
         <div className="container-fluid p-xl-2 ps-xl-5 pe-xl-5 fw-semibold">
           <div className="judul d-flex align-items-center">
-            <a
-              className="navbar-brand"
-              href="/"
-            >
+            <a className="navbar-brand" href="/">
               <h1 className="fw-bold">
                 Febe<span>Express</span>
               </h1>
@@ -33,52 +34,75 @@ function NavbarDashboard() {
           >
             <AiOutlineMenuUnfold size={35} color="#000" />
           </button>
-
         </div>
       </nav>
 
       <div id="mySidenav" className="sidenav">
-
         <ul className="navbar-nav ps-4 fw-semibold">
-          {userType === 'customer' && (
+          <li className="nav-item">
+            <Link className="nav-link d-flex gap-2" to="/dashboard">
+              <FaHome size={25} color={"#01aa5a"} />
+              Dashboard
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link d-flex gap-2" to="/dashboard">
+              <IoPersonCircleOutline size={25} color={"#01aa5a"} />
+              Profil
+            </Link>
+          </li>
+          {userType === "customer" && (
             <>
               <li className="nav-item">
-                <Link className="nav-link" to="/dashboard">
-                  Dashboard
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/dashboard">
-                  Profil
-                </Link>
-              </li>
-            </>
-          )}
-          {userType === 'admin' && (
-            <>
-              <li className="nav-item">
-                <Link className="nav-link" to="/dashboard">
+                <Link className="nav-link d-flex gap-2" to="/dashboard">
+                  <AiOutlineTruck size={25} color={"#01aa5a"} />
                   Tracking
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/dashboard">
-                  Setting
+                <Link className="nav-link d-flex gap-2" to="/dashboard">
+                  <CiBoxes size={25} color={"#01aa5a"} />
+                  Order
+                </Link>
+              </li>
+            </>
+          )}
+          {userType === "admin" && (
+            <>
+              <li className="nav-item">
+                <Link className="nav-link d-flex gap-2" to="/dashboard">
+                  <FaUsers size={25} color={"#01aa5a"} />
+                  User List
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link d-flex gap-2" to="/dashboard">
+                  <CiBoxes size={25} color={"#01aa5a"} />
+                  Order List
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link d-flex gap-2" to="/dashboard">
+                  <LuUserPlus size={25} color={"#01aa5a"} />
+                  Add Admin
                 </Link>
               </li>
             </>
           )}
           <li className="nav-item">
-            <Link className="nav-link" to="/dashboard">
+            <Link className="nav-link d-flex gap-2" to="/dashboard">
+              <MdOutlineSettings size={25} color={"#01aa5a"} />
+              Setting
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link d-flex gap-2" to="/dashboard">
+              <IoLogOutOutline size={25} color={"#01aa5a"} />
               Logout
             </Link>
           </li>
-         
-          
-
         </ul>
-        <div className="d-flex justify-content-center mt-4 ">
-        </div>
+        <div className="d-flex justify-content-center mt-4 "></div>
       </div>
     </header>
   );
@@ -87,7 +111,7 @@ function NavbarDashboard() {
 let isSidebarOpen = false;
 
 function openNav() {
-  const sidebar = document.getElementById('mySidenav');
+  const sidebar = document.getElementById("mySidenav");
   if (!isSidebarOpen) {
     if (window.innerWidth >= 400 && window.innerWidth <= 810) {
       sidebar.style.width = "60%";
@@ -110,10 +134,17 @@ function closeNav() {
   }
 }
 
-document.addEventListener('click', function(event) {
-  const sidebar = document.getElementById('mySidenav');
-  const navbarToggle = document.querySelector('.navbar-toggler');
-  if (sidebar && event.target !== sidebar && !sidebar.contains(event.target) && navbarToggle && event.target !== navbarToggle && !navbarToggle.contains(event.target)) {
+document.addEventListener("click", function (event) {
+  const sidebar = document.getElementById("mySidenav");
+  const navbarToggle = document.querySelector(".navbar-toggler");
+  if (
+    sidebar &&
+    event.target !== sidebar &&
+    !sidebar.contains(event.target) &&
+    navbarToggle &&
+    event.target !== navbarToggle &&
+    !navbarToggle.contains(event.target)
+  ) {
     closeNav();
   }
 });
