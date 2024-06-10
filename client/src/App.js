541cde3cb0d072948c1a60567a97c99ceb2fed52
adenurul13443 <adenurul073@gmail.com>
2024-06-10 14:21:29 +0700

    Perbaiki margin yang bentrok

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,11 @@ import NotFoundPage from "./pages/NotFoundPage";
 import TrackingPage from "./pages/TrackingPage";
 import OrderPage from "./dashboard/OrderPage";
 import SettingsPage from "./dashboard/SettingsPage";
-<<<<<<< HEAD
 import ShipmentDetailsPage from "./dashboard/ShipmentDetailsPage";
 import UserListPage from "./dashboard/UserListPage";
 import OrderListPage from "./dashboard/OrderListPage";
-=======
 import MyProfilePage from "./dashboard/MyProfilePage";
->>>>>>> 043531af52a1b32596b8c6508c952689ee9d0227
+
 
 function App() {
   return (
@@ -30,13 +28,10 @@ function App() {
           <Route path="/tracking" element={<TrackingPage />}/>
           <Route path="/dashboard/order" element={<OrderPage/>} />
           <Route path="/dashboard/settings" element={<SettingsPage/>} />
-<<<<<<< HEAD
           <Route path="/dashboard/shipment-details" element={<ShipmentDetailsPage/>} />
           <Route path="/dashboard/user-list" element={<UserListPage/>} />
           <Route path="/dashboard/order-list" element={<OrderListPage/>} />
-=======
           <Route path="/dashboard/myprofile" element={<MyProfilePage/>} />
->>>>>>> 043531af52a1b32596b8c6508c952689ee9d0227
         </Routes>
       </Router>
     </div>
