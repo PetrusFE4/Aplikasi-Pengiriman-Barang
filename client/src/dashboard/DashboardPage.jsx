1e3b72725812ffd2144a0b4db87a8891e72f8f77
cinta <cintaserasi3@gmail.com>
2024-06-13 02:09:21 +0700

    update halaman dashboard admin

--- client/src/dashboard/DashboardPage.jsx
+++ client/src/dashboard/DashboardPage.jsx
@@ -1,5 +1,4 @@
 import NavbarDashboard from "../componentDashboard/NavbarDashboard";
-import "../assets/css/TrackingPage.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "../assets/css/DashboardPage.css";
@@ -9,41 +8,41 @@ function DashboardPage() {
   return (
     <>
       <NavbarDashboard />
-      <div class="container mt-5">
+      <div className="container mt-5">
         {/* <p class="welcome-dashboard">Welcome, Admin1!</p> */}
-        <div class="row">
-            <div class="col-md-4 mb-4">
-                <div class="dasboard-card card no-border shadow-sm" style={{backgroundColor: '#FEF5C5', border: '1px solid   #FEF5C5'}}>
-                    <div class="card-body">
-                        <h2 id="userCount" class="display-6">0</h2>
-                        <p class="card-text">Delivered Items</p>
+        <div className="row">
+            <div className="col-md-4 mb-4">
+                <div className="dasboard-card card no-border shadow-sm" style={{backgroundColor: '#FEF5C5', border: '1px solid   #FEF5C5'}}>
+                    <div className="card-body">
+                        <h2 id="userCount" className="display-6">0</h2>
+                        <p className="card-text">Delivered Items</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 mb-4">
-                <div class="dasboard-card card no-border shadow-sm" style={{backgroundColor: '#C2EBD7', border: '1px solid  #C2EBD7'}}>
-                    <div class="card-body">
-                        <h2 id="revenue" class="display-6">0</h2>
-                        <p class="card-text">Shipped Items</p>
+            <div className="col-md-4 mb-4">
+                <div className="dasboard-card card no-border shadow-sm" style={{backgroundColor: '#C2EBD7', border: '1px solid  #C2EBD7'}}>
+                    <div className="card-body">
+                        <h2 id="revenue" className="display-6">0</h2>
+                        <p className="card-text">Shipped Items</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 mb-4">
-                <div class="dasboard-card card no-border shadow-sm" style={{backgroundColor: '#D0F6FE', border: '1px solid  #D0F6FE'}}>
-                    <div class="card-body">
-                        <h2 id="dailyVisits" class="display-6">0</h2>
-                        <p class="card-text">Pending Items</p>
+            <div className="col-md-4 mb-4">
+                <div className="dasboard-card card no-border shadow-sm" style={{backgroundColor: '#D0F6FE', border: '1px solid  #D0F6FE'}}>
+                    <div className="card-body">
+                        <h2 id="dailyVisits" className="display-6">0</h2>
+                        <p className="card-text">Pending Items</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-8 mb-4">
-                <div class="dasboard-card card no-border shadow-sm" style={{ border: '1px solid   white'}}>
-                    <div class="card-body">
+        <div className="row">
+            <div className="col-md-8 mb-4">
+                <div className="dasboard-card card no-border shadow-sm" style={{ border: '1px solid   white'}}>
+                    <div className="card-body">
                         <p><b>Latest Order</b></p>
                         <div className="table-responsive">
-                        <table class="table table-dashboard text-center">
+                        <table className="table table-dashboard text-center">
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
@@ -82,9 +81,9 @@ function DashboardPage() {
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 mb-4">
-                <div class="dasboard-card card no-border shadow-sm" style={{ border: '1px solid   white'}}>
-                    <div class="card-body text-center">
+            <div className="col-md-4 mb-4">
+                <div className="dasboard-card card no-border shadow-sm" style={{ border: '1px solid   white'}}>
+                    <div className="card-body text-center">
                         <p><b>Role</b></p>
                         <p>Admin</p>
                         <hr />
