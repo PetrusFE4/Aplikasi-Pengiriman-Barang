de6c1cb02209748089963a2fd1be2fe9bbbb58e4
ajiekusumadhany <ajieikruskusumadhany@gmail.com>
2024-06-12 20:11:31 +0700

    perbaiki navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,51 +4,41 @@ import "../assets/css/Navbar.css";
 import { ButtonStyle } from "./StyledComponents";
 import { IoIosCloseCircle } from "react-icons/io";
 import { AiOutlineMenuUnfold } from "react-icons/ai";
-import { Link, useNavigate, useLocation } from "react-router-dom";
-import { useState, useEffect, useCallback } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { flushSync } from "react-dom";
 import {jwtDecode} from "jwt-decode"; // Perbaiki import untuk jwtDecode
 import Swal from "sweetalert2";
 
 function Header() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isAdmin, setIsAdmin] = useState(false); // Tambahkan state untuk peran admin
+  const [isAdmin, setIsAdmin] = useState(false);
   const navigate = useNavigate();
-  const location = useLocation();
 
-  // ini buat scroll menu
-  const handleScroll = useCallback(() => {
-    const header = document.querySelector(".header");
-    if (header && header.contains) {
-      if (window.scrollY > 100) {
-        header.classList.add("scrolled");
-      } else {
-        header.classList.remove("scrolled");
-      }
-    }
-  }, []);
-
-  const scrollTo = sessionStorage.getItem("scrollTo");
-  if (scrollTo) {
-    const element = document.getElementById(scrollTo);
-    if (element) {
-      flushSync(() => {
-        const yOffset = -120; // Mengatur offset jarak
-        const y =
-          element.getBoundingClientRect().top + window.scrollY + yOffset;
-        window.scrollTo({ top: y, behavior: "smooth" });
-      });
-      sessionStorage.removeItem("scrollTo");
-    }
-  }
+  const handleScroll = () => {
+    console.log("Scrolling...");
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
 
+    const scrollTo = sessionStorage.getItem("scrollTo");
+    if (scrollTo) {
+      const element = document.getElementById(scrollTo);
+      if (element) {
+        flushSync(() => {
+          const yOffset = -120;
+          const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
+          window.scrollTo({ top: y, behavior: "smooth" });
+        });
+        sessionStorage.removeItem("scrollTo");
+      }
+    }
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [handleScroll, location]);
+  }, []);
 
   const handleClick = (e, path, hash) => {
     e.preventDefault();
@@ -56,15 +46,14 @@ function Header() {
     navigate(path);
   };
 
-  // mengecek sudah login atau belum
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
       try {
         const decodedToken = jwtDecode(token);
         if (decodedToken) {
-          setIsLoggedIn(true); // Set status login jika token valid
-          setIsAdmin(decodedToken.role === "admin"); // Set status admin jika peran adalah admin
+          setIsLoggedIn(true);
+          setIsAdmin(decodedToken.role === "admin");
         }
       } catch (error) {
         console.error("Token decoding error:", error);
@@ -72,7 +61,6 @@ function Header() {
     }
   }, []);
 
-  // ini buat kalau user klik menu order tapi belum login, nanti diarahin kehalaman login
   const handleClickOrder = (e) => {
     const token = localStorage.getItem("token");
     if (!token) {
@@ -102,72 +90,37 @@ function Header() {
 
   return (
     <header className="header">
-      <nav
-        className="navbar navbar-expand-lg pt-1 pb-1 pt-sm-0 pb-sm-0"
-        id="nav-dekstop"
-      >
+      <nav className="navbar navbar-expand-lg pt-1 pb-1 pt-sm-0 pb-sm-0" id="nav-dekstop">
         <div className="container-fluid p-xl-2 ps-xl-5 pe-xl-5 fw-semibold">
           <div className="judul d-flex align-items-center">
-            <a
-              className="navbar-brand"
-              to="/"
-              onClick={(e) => handleClick(e, "/", "home")}
-            >
-              <h1 className="fw-bold">
-                Febe<span>Express</span>
-              </h1>
+            <a className="navbar-brand" href="/">
+              <h1 className="fw-bold">Febe<span>Express</span></h1>
             </a>
           </div>
-          <button
-            className="navbar-toggler custom-toggler"
-            type="button"
-            aria-expanded="false"
-            onClick={openNav}
-          >
+          <button className="navbar-toggler custom-toggler" type="button" aria-expanded="false" onClick={openNav}>
             <AiOutlineMenuUnfold size={35} color="#000" />
           </button>
-          <div
-            className="collapse navbar-collapse"
-            style={{ boxSizing: "border-box" }}
-          >
+          <div className="collapse navbar-collapse" style={{ boxSizing: "border-box" }}>
             <ul className="navbar-nav mx-lg-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  to="/"
-                  onClick={(e) => handleClick(e, "/", "home")}
-                >
+                <Link className="nav-link" to="/">
                   Home
                 </Link>
               </li>
               <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle"
-                  to="/#order"
-                  id="navbarDropdownHome"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
+                <Link className="nav-link dropdown-toggle" to="/#order" id="navbarDropdownHome" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Order
                 </Link>
-                <ul
-                  className="dropdown-menu"
-                  aria-labelledby="navbarDropdownHome"
-                >
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdownHome">
                   <li>
                     <Link className="dropdown-item" to="/shipping-rates">
                       Shipping Rates
                     </Link>
                   </li>
                   <li>
-                    <a
-                      className="dropdown-item"
-                      href="#"
-                      onClick={handleClickOrder}
-                    >
+                    <Link className="dropdown-item" href="#" onClick={handleClickOrder}>
                       Order
-                    </a>
+                    </Link>
                   </li>
                 </ul>
               </li>
@@ -177,29 +130,15 @@ function Header() {
                 </Link>
               </li>
               <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  to="/"
-                  onClick={(e) => handleClick(e, "/", "service-section")}
-                >
+                <Link className="nav-link" to="/" onClick={(e) => handleClick(e, "/", "service-section")}>
                   Service
                 </Link>
               </li>
               <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle"
-                  to="/#information"
-                  id="navbarDropdownHome"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
+                <Link className="nav-link dropdown-toggle" to="/#information" id="navbarDropdownHome" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Information
                 </Link>
-                <ul
-                  className="dropdown-menu"
-                  aria-labelledby="navbarDropdownHome"
-                >
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdownHome">
                   <li>
                     <Link className="dropdown-item" to="/support-center">
                       Support Center
@@ -211,11 +150,7 @@ function Header() {
                     </Link>
                   </li>
                   <li>
-                    <Link
-                      className="dropdown-item"
-                      to="/"
-                      onClick={(e) => handleClick(e, "/", "faq-section")}
-                    >
+                    <Link className="dropdown-item" to="/" onClick={(e) => handleClick(e, "/", "faq-section")}>
                       FAQ
                     </Link>
                   </li>
@@ -223,10 +158,7 @@ function Header() {
               </li>
             </ul>
             <div className="nav-button-container">
-              <Link
-                to={isLoggedIn ? (isAdmin ? "/dashboard/order-list" : "/dashboard/order") : "/login"}
-                style={{ textDecoration: "none" }}
-              >
+              <Link to={isLoggedIn ? (isAdmin ? "/dashboard/order-list" : "/dashboard/order") : "/login"} style={{ textDecoration: "none" }}>
                 <ButtonStyle className="nav-button">
                   {isLoggedIn ? "Dashboard" : "Login"}
                 </ButtonStyle>
@@ -249,12 +181,7 @@ function Header() {
             </Link>
           </li>
           <li className="nav-item dropdown">
-            <Link
-              className="nav-link dropdown-toggle"
-              to="/#ordermenu"
-              data-bs-toggle="collapse"
-              onClick={handleClickOrder}
-            >
+            <Link className="nav-link dropdown-toggle" to="/#ordermenu" data-bs-toggle="collapse" onClick={handleClickOrder}>
               Order
             </Link>
             <ul className="nav collapse" id="ordermenu" data-bs-parent="menu">
@@ -264,13 +191,9 @@ function Header() {
                 </Link>
               </li>
               <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#"
-                  onClick={handleClickOrder}
-                >
+                <Link className="nav-link" href="#" onClick={handleClickOrder}>
                   Order
-                </a>
+                </Link>
               </li>
             </ul>
           </li>
@@ -285,18 +208,10 @@ function Header() {
             </Link>
           </li>
           <li className="nav-item dropdown">
-            <Link
-              className="nav-link dropdown-toggle"
-              to="/#informationmenu"
-              data-bs-toggle="collapse"
-            >
+            <Link className="nav-link dropdown-toggle" to="/#informationmenu" data-bs-toggle="collapse">
               Information
             </Link>
-            <ul
-              className="nav collapse"
-              id="informationmenu"
-              data-bs-parent="menu"
-            >
+            <ul className="nav collapse" id="informationmenu" data-bs-parent="menu">
               <li className="nav-item">
                 <Link className="nav-link" to="/support-center">
                   Support Center
@@ -316,14 +231,8 @@ function Header() {
           </li>
         </ul>
         <div className="d-flex justify-content-center mt-4 ">
-          <Link
-            to={isLoggedIn ? (isAdmin ? "/dashboard/order-list" : "/dashboard/order") : "/login"}
-            style={{ textDecoration: "none" }}
-          >
-            <ButtonStyle
-              className="sidenav-button"
-              style={{ padding: "8px 100px" }}
-            >
+          <Link to={isLoggedIn ? (isAdmin ? "/dashboard/order-list" : "/dashboard/order") : "/login"} style={{ textDecoration: "none" }}>
+            <ButtonStyle className="sidenav-button" style={{ padding: "8px 100px" }}>
               {isLoggedIn ? "Dashboard" : "Login"}
             </ButtonStyle>
           </Link>
