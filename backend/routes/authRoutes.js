7c516ea64ce8e84e3544c57c09fae47fda511ad7
taufiik <mohamadtaufik0413@gmail.com>
2024-06-20 12:24:38 +0700

    Menambah fitur tracking

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,6 +4,9 @@ const authController = require('../controller/authController');
 const adminController = require('../controller/adminController');
 const authToken = require('../middleware/authentications')
 
+//const status
+const {updateStatus, trackOrder} = require('../controller/statusController');
+
 const hitungOngkir = require('../controller/ongkirController');
 const buatPesanan = require('../controller/orderController');
 
@@ -13,5 +16,8 @@ router.post('/logout',  authToken, authController.logout);
 router.post('/registerAdmin', adminController.registerAdmin);
 router.post('/cek-ongkir', hitungOngkir);
 router.post('/order', buatPesanan);
+
+router.get('/status', updateStatus);
+router.get('/track/:order_id', trackOrder );
 
 module.exports = router;
